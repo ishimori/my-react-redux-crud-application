66b7d715f713bbd1cbabc45d2c02cb61eed2838e
ishimori_yuzuru <ishimori_yuzuru@probsc.jp>
2020-12-30 07:17:29 +0900

    learn jsx

--- helloworld3/src/App.js
+++ helloworld3/src/App.js
@@ -1,9 +1,16 @@
-import React , {Component} from 'react';
+import React,{Component} from 'react';
 
 
 class App extends Component {
   render(){
-    return <h1>Hello,World2!</h1>
+    return(
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onClick={() => console.log("i am click")} />
+
+      </React.Fragment>
+
+    )
   }
 }
 
