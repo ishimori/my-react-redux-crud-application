158b5357242a070dd4b8c3529a606a6f25d0114c
ishimori_yuzuru <ishimori_yuzuru@probsc.jp>
2020-12-30 06:54:38 +0900

    commit more

--- helloworld3/src/App.js
+++ helloworld3/src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Hello,world!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render(){
+    return <div><h1>Hello,World2!</h1></div>
+  }
 }
 
 export default App;
