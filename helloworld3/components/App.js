8b502804900aa73fcf687189f40eeb2619a27517
ishimori_yuzuru <ishimori_yuzuru@probsc.jp>
2020-12-31 16:16:29 +0900

    arekore

---
+++ helloworld3/components/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+const App = () =>(<Counter></Counter>)
+
+class Counter extends Component{
+
+  constructor(props){
+    super(props)
+    this.state = {count : 0}
+    console.log(this.state)
+  }
+
+  handlePlusButton = () => {
+    console.log("handluPlusButton");
+    this.setState({count : this.state.count + 1})
+  }
+  handleMinusButton = () => {
+    console.log("handluMinusButton");
+    this.setState({count : this.state.count - 1})
+  }
+
+  render () {
+    console.log(this.state)
+    return (
+      <React.Fragment>
+        <div>count : {this.state.count} </div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
+}
+
+
+export default App;
